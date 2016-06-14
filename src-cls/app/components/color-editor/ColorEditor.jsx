35826a767b5d1713f3486f03c4667d5eca38f869
Artem Govorov <artem.govorov@gmail.com>
2016-06-14 23:08:53 +1000

    More tests

--- src-cls/app/components/color-editor/ColorEditor.jsx
+++ src-cls/app/components/color-editor/ColorEditor.jsx
@@ -1,5 +1,8 @@
 'use strict'
 import React, { Component } from 'react'
+import {col2rgba} from 'utils/color-utils'
+
+console.log(col2rgba);
 
 export default class ColorEditor extends Component {
   render() {
---
+++ src-cls/app/utils/color-utils.js
@@ -0,0 +1,1 @@
+export function col2rgba() {}
